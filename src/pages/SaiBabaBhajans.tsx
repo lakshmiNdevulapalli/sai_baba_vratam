0fd18618031edadde457e80207591589b8a21065
Balu Devulapalli <baludevulapalli@BaluNiharikas-MacBook-Pro.local>
2024-11-02 23:03:39 -0700

    updated bhajans mp4 url

--- src/pages/SaiBabaBhajans.tsx
+++ src/pages/SaiBabaBhajans.tsx
@@ -17,7 +17,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Shlokam",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-1-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-1-of-8wmv.mp4",
       coverImage: "src/assets/sai-baba-bhajan.jpeg",
       singers: "Smt. Devulapalli Srinivasa Sridevi",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -26,7 +27,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Gurudwaarame",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-2-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-2-of-8wmv.mp4",
       coverImage: "src/assets/sai_baba_bhajan_3.jpg",
       singers: "Smt. Devulapalli Srinivasa Sridevi & Chakradhar",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -35,7 +37,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Karuna Prema",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-3-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-3-of-8wmv.mp4",
       coverImage: "src/assets/Bhajans.jpg",
       singers: "Smt. Devulapalli Srinivasa Sridevi & Siva Sharma",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -44,7 +47,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Ekaadasa Sutralu",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-4-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-4-of-8wmv.mp4",
       coverImage: "src/assets/bhajan_2.jpg",
       singers: "Smt. Devulapalli Srinivasa Sridevi",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -53,7 +57,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Madhuramaya",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-5-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-5-of-8wmv.mp4",
       coverImage: "src/assets/sai_bhajan_5.jpg",
       singers: "Smt. Devulapalli Srinivasa Sridevi & Chakradhar",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -62,7 +67,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Satyam Sivam Sundaram",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-6-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-6-of-8wmv.mp4",
       coverImage: "src/assets/krishna_bhajan.jpg",
       singers: "Smt. Devulapalli Srinivasa Sridevi & Chakradhar",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -71,7 +77,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Neellathoti Deepam",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-7-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-7-of-8wmv.mp4",
       coverImage: "src/assets/Baba_blissful.png",
       singers: "Smt. Devulapalli Srinivasa Sridevi & Chakradhar",
       lyricist: "Aakella Surya Narayana Murthy",
@@ -80,7 +87,8 @@ const SaiBabaBhajans: React.FC = () => {
     {
       title: "Mangalam Sri Sai",
       description: "A soulful bhajan celebrating Sai Baba's blessings.",
-      mp4Url: "src/bhajans/sadguru-sai-ananda-daayi-part-8-of-8wmv.mp4",
+      mp4Url:
+        "https://sai-baba-vratam-bhajans-and-assets.s3.us-east-1.amazonaws.com/sadguru-sai-ananda-daayi-part-8-of-8wmv.mp4",
       coverImage: "src/assets/sai_bhajan_2.jpg",
       singers: "Smt. Devulapalli Srinivasa Sridevi & Chakradhar",
       lyricist: "Aakella Surya Narayana Murthy",
