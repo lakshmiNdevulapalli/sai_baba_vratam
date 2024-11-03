bbedd41b0731e75a40c0ea78de3f4f4341c84794
Balu Devulapalli <baludevulapalli@BaluNiharikas-MacBook-Pro.local>
2024-11-02 22:42:50 -0700

    Updated Tab css and copyright css

--- src/pages/Tabs.tsx
+++ src/pages/Tabs.tsx
@@ -6,64 +6,80 @@ const Tabs: React.FC = () => {
   const handleTabClick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
-    <div className="tabs">
-      {location.pathname !== "/" && (
+    <div>
+      <div className="tabs">
+        {location.pathname !== "/" && (
+          <Link
+            to="/"
+            className="tab-button home-button"
+            onClick={handleTabClick}
+          >
+            Home
+          </Link>
+        )}
         <Link
-          to="/"
-          className="tab-button home-button"
+          to="/vratamDetails"
+          className={`tab-button ${
+            location.pathname === "/vratamDetails" ? "active" : ""
+          }`}
           onClick={handleTabClick}
         >
-          Home
+          Sai Baba Vratam Details
         </Link>
-      )}
-      <Link
-        to="/vratamDetails"
-        className={`tab-button ${location.pathname === "/vratamDetails" ? "active" : ""}`}
-        onClick={handleTabClick}
-      >
-        Sai Baba Vratam Details
-      </Link>
-      <Link
-        to="/astottaram"
-        className={`tab-button ${location.pathname === "/astottaram" ? "active" : ""}`}
-        onClick={handleTabClick}
-      >
-        Ashtottaram (108 names)
-      </Link>
-      <Link
-        to="/saiBabaVrataKatha"
-        className={`tab-button ${location.pathname === "/saiBabaVrataKatha" ? "active" : ""}`}
-        onClick={handleTabClick}
-      >
-        Vratakatha (Short Stories)
-      </Link>
-      <Link
-        to="/aarti"
-        className={`tab-button ${location.pathname === "/aarti" ? "active" : ""}`}
-        onClick={handleTabClick}
-      >
-        Mangala Aarti
-      </Link>
-      <Link
-        to="/saiBabaBhajans"
-        className={`tab-button ${location.pathname === "/saiBabaBhajans" ? "active" : ""}`}
-        onClick={handleTabClick}
-      >
-        Sai Baba Bhajans
-      </Link>
+        <Link
+          to="/astottaram"
+          className={`tab-button ${
+            location.pathname === "/astottaram" ? "active" : ""
+          }`}
+          onClick={handleTabClick}
+        >
+          Ashtottaram (108 names)
+        </Link>
+        <Link
+          to="/saiBabaVrataKatha"
+          className={`tab-button ${
+            location.pathname === "/saiBabaVrataKatha" ? "active" : ""
+          }`}
+          onClick={handleTabClick}
+        >
+          Vratakatha (Short Stories)
+        </Link>
+        <Link
+          to="/aarti"
+          className={`tab-button ${
+            location.pathname === "/aarti" ? "active" : ""
+          }`}
+          onClick={handleTabClick}
+        >
+          Mangala Aarti
+        </Link>
+        <Link
+          to="/saiBabaBhajans"
+          className={`tab-button ${
+            location.pathname === "/saiBabaBhajans" ? "active" : ""
+          }`}
+          onClick={handleTabClick}
+        >
+          Sai Baba Bhajans
+        </Link>
+      </div>
 
-      {/* Copyright Notice */}
-      <footer className="copyright-footer">
-        <p>
-          Copyright © {new Date().getFullYear()} Venkata Lakshmi Narasimha,
-          Devulapalli . All rights reserved.
-        </p>
-        <p>
-          Contact:{" "}
-          <a href="mailto:your-email@example.com">baludevulapalli@gmail.com</a>
-        </p>
-      </footer>
+      <div className="footer-container">
+        <footer>
+          <p>
+            Copyright © 2024 Venkata Lakshmi Narasimha, Devulapalli. All rights
+            reserved.
+          </p>
+          <p>
+            Contact:{" "}
+            <a href="mailto:baludevulapalli@gmail.com">
+              baludevulapalli@gmail.com
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 };
